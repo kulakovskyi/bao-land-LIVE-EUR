fdd9dfe91e9956b9ec5eeb702456c87208bc6d51
kulakovskyi <torchok9@gmail.com>
2022-09-15 20:00:56 +0300

    desc 1920

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,1 @@
-"use strict";
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiLCJzZWNvbmQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUNBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiIiwiIl19
+"use strict";!function(){var e;1280<=window.innerWidth&&(e=document.getElementById("scene"),new Parallax(e))}();
---
+++ front/js/main.js
@@ -0,0 +1,18 @@
+
+(function(){
+    let screenWidth = window.innerWidth
+    if (screenWidth >= 1280){
+        let scene = document.getElementById('scene')
+        let parallaxInstance = new Parallax(scene)
+    }
+}());
+
+
+
+
+
+
+
+
+
+
