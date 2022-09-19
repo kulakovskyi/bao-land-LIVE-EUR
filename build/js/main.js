ac602fc108d968a38bed9328b1c921c5492bc200
kulakovskyi <torchok9@gmail.com>
2022-09-19 16:06:43 +0300

    add js

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-"use strict";!function(){var e;1280<=window.innerWidth&&(e=document.getElementById("scene"),new Parallax(e))}();
+"use strict";!function(){var e;1280<=window.innerWidth&&(e=document.getElementById("scene"),new Parallax(e))}(),function(){var e=new URL(window.location.href);e.searchParams.has("qtag")&&localStorage.setItem("qtag",e.searchParams.get("qtag")),window.addEventListener("click",function(e){var t=e.target.closest("a");t&&(e.preventDefault(),(e=new URL(t.href)).searchParams.set("qtag",localStorage.getItem("qtag")),document.location.href=e)})}();
